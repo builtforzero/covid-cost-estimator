2a96211f5a21e6b5dbd4f7c5947a678848b64b0b
koffeeya <kavya.beheraj@gmail.com>
2020-04-29 12:31:21 -0400

    Added calculator function and bootstrap scripts

--- main.js
+++ main.js
@@ -1,46 +1,84 @@
-/* CONSTANTS & GLOBALS */
-/* Variables we want access to throughout the application */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
-  margin = {
-    top: 10,
-    bottom: 10,
-    left: 10,
-    right: 10
-  };
+/* VARIABLES */
 
-let svg;
-
-/* APPLICATION STATE */
-/* Variables that define the current state of the application */
+// Application state set to default values
 let state = {
-    data: null,
-    community: null,
-    email: null,
-    months: 0,
-    homelessInd: 0,
-    chronicInd: 0,
-    fiftyplusInd: 0,
-    infected: 0,
-    costQI: 0,
-    costPH: 0
+  // Community information
+  community: null,
+  email: null,
+  // Form inputs
+  homelessNumber: 20, // Number of people experiencing homelessness to model
+  costPerBedQI: 10, // Cost per bed for Q&I
+  costPerBedPP: 5, // Cost per bed for Permanent Placement
+  percentQI: 0.75, // Percent of beds that will be Q&I
+  percentPP: 0.25, // Percent of beds that will be Permanent Placement
+  percentInfected: 0.5, // Percent infected by COVID-19
+  // Calculated values
+  bedsTotal: 0, // Total beds needed
+  bedsQI: 0, // Number of Q&I beds needed
+  bedsPP: 0, // Number of Permanent Placement beds needed
+  costQI: 0, // Cost for all Q&I beds needed
+  costPP: 0, // Cost for all Permanent Placement beds needed
+  costTotal: 0 // Overall total cost
 };
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzB4VKR9uSm83s0CFHUaMBUV611o4d24-NmQIfPFIhqFOh10qw/exec'
 
-/* LOAD DATA */
-console.log("current state:", state);
-init();
+const form = document.forms['submitToGoogleSheet']
 
-/* INIT */
-/* This function will be run once when the data finishes loading in */
-function init() {
-    console.log("initializing!")
+console.log("Starting State", state)
 
-    draw();
+
+/* UPDATE FUNCTIONS */
+/* Update values and set event listeners */
+
+// Utility function to update the global state (can be used in event listeners)
+function setGlobalState(nextState) {
+  state = {
+    ...state,
+    ...nextState
+  }
+};
+
+// Calculate values from form inputs
+function recalculate() {
+  setGlobalState({
+    bedsTotal: state.homelessNumber * state.percentInfected,
+    bedsQI: (state.homelessNumber * state.percentInfected) * state.percentQI,
+    bedsPP: (state.homelessNumber * state.percentInfected) * state.percentPP,
+    costQI: ((state.homelessNumber * state.percentInfected) * state.percentQI) * state.costPerBedQI,
+    costPP: ((state.homelessNumber * state.percentInfected) * state.percentPP) * state.costPerBedPP,
+    costTotal: (((state.homelessNumber * state.percentInfected) * state.percentQI) * state.costPerBedQI) + (((state.homelessNumber * state.percentInfected) * state.percentPP) * state.costPerBedPP)
+  })
+  console.log("Recalculated State", state)
 }
 
-/* DRAW */
-/* This function will run every time there is an update to the data or state */
-function draw() {
-    console.log("drawing!")
+recalculate();
+
+// Add event listeners to each form input
+
+
+
+
+
+
+/* DISPLAY & SUBMIT FUNCTIONS */
+/* Display values */
+
+// Select display divs. Append state values
+
+
+// Submit form data to Google Sheets. Takes script URL and form object as arguments
+function submitData(scriptURL, form) {
+  form.addEventListener('submit', e => {
+    console.log("Submitting Data!")
+    e.preventDefault()
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form)
+      })
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
 }
+
+submitData(scriptURL, form);
